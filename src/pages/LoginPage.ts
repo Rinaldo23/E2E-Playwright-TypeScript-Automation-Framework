9f493afbfb6aa812a37601f6f5d755ce813e0f03
Rinaldo23 <rinaldobadigar@gmail.com>
2025-07-26 00:48:19 +0530

    Add new Login page class

---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import { Locator, Page } from '@playwright/test';
+
+export default class LoginPage {
+
+    private readonly page: Page;
+    private readonly emailTxtBox: Locator;
+    private readonly passwordTxtBox: Locator;
+    private readonly loginBtn: Locator;
+    private readonly myAccountTxt: Locator;
+
+    constructor(page: Page) {
+        this.emailTxtBox = this.page.getByRole('textbox', { name: 'E-Mail Address' });
+        this.passwordTxtBox = this.page.getByRole('textbox', { name: 'Password' });
+        this.loginBtn = this.page.getByRole('button', { name: 'Login' });
+        this.myAccountTxt = this.page.getByRole('heading', { name: 'My Account' });
+    }
+
+    async login(email: string, password: string) {
+        this.emailTxtBox.fill(email);
+        this.passwordTxtBox.fill(password);
+        this.loginBtn.click();
+    }
+
+    async isLoginSuccessful() {
+        return this.myAccountTxt.isVisible();
+    }
+
+}
