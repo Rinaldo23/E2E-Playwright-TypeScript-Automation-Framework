10441c92e3b9c11b058dae128ddfeabb422c3553
Rinaldo23 <rinaldobadigar@gmail.com>
2025-07-27 21:04:31 +0530

    Update: existing tests to replace page object creation with pom-fixtures

--- src/tests/login-module/login-positive-tests.spec.ts
+++ src/tests/login-module/login-positive-tests.spec.ts
@@ -1,18 +1,13 @@
-import { test, expect } from '@playwright/test';
-import HomePage from '../../pages/HomePage';
-import LoginPage from '../../pages/LoginPage';
+import { test, expect } from '../../fixtures/pom-fixture';
 
 test.describe("Login Scenario", {}, async () => {
 
-    test("Login with valid user credentails", async ({ page }) => {
+    test("Login with valid user credentails", async ({ page, homePage, loginPage }) => {
 
         await page.goto("https://ecommerce-playground.lambdatest.io/index.php?route=common/home");
         await expect(page).toHaveTitle("Your Store");
 
-        const homePage = new HomePage(page);
         await homePage.navigateToLoginModule();
-
-        const loginPage = new LoginPage(page);
         await loginPage.login("virat.kolhi@gmail.com", "Test@12345");
 
         await page.waitForURL("**/account");
